e147f725ad6a53a1f55097e405aab3fea469757c
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2022-09-21 16:15:26 +0200

    Changed base type of Tree

--- be.MbDevelopment.Greenmaster.Models/Arboretum/PlantTypes/Tree.cs
+++ be.MbDevelopment.Greenmaster.Models/Arboretum/PlantTypes/Tree.cs
@@ -2,7 +2,7 @@
 
 namespace be.MbDevelopment.Greenmaster.Models.Arboretum.PlantTypes;
 
-public class Tree<T> : PlantUseBase<T> where T : Specie
+public class Tree<T> : PlantType<T> where T : Specie
 {
     public Tree(T specie) : base(specie)
     {
