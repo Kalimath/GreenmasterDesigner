2d4c03d29077c96c8149f7ec1b909aee16103cc5
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2022-09-22 12:52:49 +0200

    Moved Threshold methods to PlantThresholds.cs

--- be.MbDevelopment.Greenmaster.Models/Entities/Arboretum/PlantThresholds.cs
+++ be.MbDevelopment.Greenmaster.Models/Entities/Arboretum/PlantThresholds.cs
@@ -9,21 +9,68 @@ public class PlantThresholds
     public double MetricDiameterMin { get; set; }
     public double MetricDiameterMax { get; set; }
 
-    public bool ZeroMeansNoRestriction { get; }
+    public bool ZeroMeansAny { get; }
     public bool Hedgeable { get; }
     public Lifecycle Cycle { get; }
     
-    public PlantThresholds(bool zeroMeansNoRestriction,Lifecycle cycle, double metricHeightMax, double metricHeightMin,
+    public PlantThresholds(bool zeroMeansAny,Lifecycle cycle, double metricHeightMax, double metricHeightMin,
         double metricDiameterMin, double metricDiameterMax, bool hedgeable)
     {
         Hedgeable = hedgeable;
-        ZeroMeansNoRestriction = zeroMeansNoRestriction;
+        ZeroMeansAny = zeroMeansAny;
         Cycle = cycle;
         MetricHeightMax = metricHeightMax;
         MetricHeightMin = metricHeightMin;
         MetricDiameterMin = metricDiameterMin;
         MetricDiameterMax = metricDiameterMax;
     }
-    
-    
+
+    public bool SpecieMeetsThresholds(Specie specie)
+    {
+        return SpeciePropertiesMeetThreshold(specie) && SpecieDimensionsMeetThreshold(specie);
+    }
+
+    private bool SpecieDimensionsMeetThreshold(Specie specie)
+    {
+        bool res = true;
+        
+        var metricHeight = specie.Dimensions.MetricHeight;
+        var metricDiameter = specie.Dimensions.MetricDiameter;
+        
+        bool heightMeetsThreshold = MeetsMaxThreshold(MetricHeightMax, metricHeight) &&
+                                    MeetsMinThreshold(MetricHeightMin, metricHeight);
+
+        bool diameterMeetsThreshold = MeetsMaxThreshold(MetricDiameterMax, metricDiameter) &&
+                                      MeetsMinThreshold(MetricDiameterMin, metricDiameter);
+
+        if (!heightMeetsThreshold || !diameterMeetsThreshold)
+        {
+            res = false;
+        }
+
+        return res;
+    }
+
+    private bool SpeciePropertiesMeetThreshold(Specie specie)
+    {
+        bool res = false;
+        var specieProperties = specie.Properties;
+
+        if (specieProperties.Hedgeable == Hedgeable && specieProperties.Cycle == Cycle)
+        {
+            res = true;
+        }
+
+        return res;
+    }
+
+    public bool MeetsMaxThreshold(double threshold, double validatable)
+    {
+        return (ZeroMeansAny && threshold == 0) || validatable <= threshold;
+    }
+
+    public bool MeetsMinThreshold(double threshold, double validatable)
+    {
+        return (ZeroMeansAny && threshold == 0) || validatable >= threshold;
+    }
 }
--- be.MbDevelopment.Greenmaster.Models/Entities/Arboretum/PlantTypes/PlantType.cs
+++ be.MbDevelopment.Greenmaster.Models/Entities/Arboretum/PlantTypes/PlantType.cs
@@ -12,7 +12,7 @@ public abstract class PlantType : PlantUseBase
         Thresholds = thresholds;
         try
         {
-            if (!SpecieMeetsThresholds(specie))
+            if (!Thresholds.SpecieMeetsThresholds(specie))
             {
                 throw new ThresholdException(
                     $"Specie \"{specie.ScientificName}\" does not meet requirements to be a {this.GetType().Name.ToLower()}.");
@@ -23,54 +23,5 @@ public abstract class PlantType : PlantUseBase
             Console.WriteLine(e.StackTrace);
             throw;
         }
-    }
-
-    private bool SpecieMeetsThresholds(Specie specie)
-    {
-        return SpeciePropertiesMeetThreshold(specie) && SpecieDimensionsMeetThreshold(specie);
-    }
-
-    private bool SpecieDimensionsMeetThreshold(Specie specie)
-    {
-        bool res = true;
-        
-        var metricHeight = specie.Dimensions.MetricHeight;
-        var metricDiameter = specie.Dimensions.MetricDiameter;
-        var zeroMeansAny = Thresholds.ZeroMeansNoRestriction;
-        bool heightMeetsThreshold = MeetsMaxThreshold(Thresholds.MetricHeightMax, metricHeight, zeroMeansAny) &&
-                                    MeetsMinThreshold(Thresholds.MetricHeightMin, metricHeight, zeroMeansAny);
-
-        bool diameterMeetsThreshold = MeetsMaxThreshold(Thresholds.MetricDiameterMax, metricDiameter, zeroMeansAny) &&
-                                      MeetsMinThreshold(Thresholds.MetricDiameterMin, metricDiameter, zeroMeansAny);
-
-        if (!heightMeetsThreshold || !diameterMeetsThreshold)
-        {
-            res = false;
-        }
-
-        return res;
-    }
-
-    private bool SpeciePropertiesMeetThreshold(Specie specie)
-    {
-        bool res = false;
-        var specieProperties = specie.Properties;
-
-        if (specieProperties.Hedgeable == Thresholds.Hedgeable && specieProperties.Cycle == Thresholds.Cycle)
-        {
-            res = true;
-        }
-
-        return res;
-    }
-
-    public bool MeetsMaxThreshold(double threshold, double validatable, bool zeroMeansAny)
-    {
-        return (zeroMeansAny && threshold == 0) || validatable <= threshold;
-    }
-
-    public bool MeetsMinThreshold(double threshold, double validatable, bool zeroMeansAny)
-    {
-        return (zeroMeansAny && threshold == 0) || validatable >= threshold;
     }
 }
