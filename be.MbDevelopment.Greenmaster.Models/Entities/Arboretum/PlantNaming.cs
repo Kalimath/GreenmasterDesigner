68dda5be0857b9a5060eb00f57050a7156a8a6fc
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2022-09-23 13:01:55 +0200

    PlantNaming implemented & tested

---
+++ be.MbDevelopment.Greenmaster.Models/Entities/Arboretum/PlantNaming.cs
@@ -0,0 +1,28 @@
+﻿using be.MbDevelopment.Greenmaster.Extensions.SubTypes;
+using be.MbDevelopment.Greenmaster.Models.StaticData;
+
+namespace be.MbDevelopment.Greenmaster.Models.Entities.Arboretum;
+
+public class PlantNaming
+{
+    public string Genus { get; }
+    public string Specie { get; }
+    public EnumVDictionary<Language, string> Common { get; }
+
+    public PlantNaming(string genus, string specie, EnumVDictionary<Language, string> common)
+    {
+        this.Genus = !string.IsNullOrWhiteSpace(genus)
+            ? genus
+            : throw new ArgumentException("Given Genus-name is not valid");
+
+        this.Specie = !string.IsNullOrWhiteSpace(specie)
+            ? specie
+            : throw new ArgumentException("Given Genus-name is not valid");
+        this.Common = common;
+    }
+
+    public string GetScientificName()
+    {
+        return $"{Genus} {Specie}";
+    }
+}
---
+++ be.MbDevelopment.Greenmaster.Tests/UnitTests/Models/Entities/Arboretum/PlantNamingShould.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.AccessControl;
+using be.MbDevelopment.Greenmaster.Extensions.SubTypes;
+using be.MbDevelopment.Greenmaster.Models.Entities.Arboretum;
+using be.MbDevelopment.Greenmaster.Models.StaticData;
+using Xunit;
+
+namespace be.MbDevelopment.Greenmaster.Tests.UnitTests.Models.Entities.Arboretum;
+
+public class PlantNamingShould
+{
+    [Fact]
+    public void CorrectlySetPropsWhenCtorIsCalled()
+    {
+        var translations = new EnumVDictionary<Language, string>();
+        translations[Language.Nl.ToString()] = "Gewone buxus";
+        const string genus = "Buxus";
+        const string specie = "Sempervirens";
+        string scientificName = $"{genus} {specie}";
+        PlantNaming plantNaming = new PlantNaming(genus: genus, specie: specie,
+            common: translations);
+        Assert.Equal(scientificName, plantNaming.GetScientificName());
+    }
+    
+    [Fact]
+    public void ThrowsWhenCtorParamNamesAreEmpty()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var translations = new EnumVDictionary<Language, string>();
+            translations[Language.Nl.ToString()] = "Gewone buxus";
+            const string genus = "";
+            const string specie = "Sempervirens";
+            string scientificName = $"{genus} {specie}";
+            PlantNaming plantNaming = new PlantNaming(genus: genus, specie: specie,
+                common: translations);
+        });
+    }
+    
+    [Fact]
+    public void ThrowsWhenCtorParamNamesArenull()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var translations = new EnumVDictionary<Language, string>();
+            translations[Language.Nl.ToString()] = "Gewone buxus";
+            const string genus = "Buxus";
+            const string specie = null!;
+            string scientificName = $"{genus} {specie}";
+            PlantNaming plantNaming = new PlantNaming(genus: genus, specie: specie,
+                common: translations);
+        });
+    }
+}
