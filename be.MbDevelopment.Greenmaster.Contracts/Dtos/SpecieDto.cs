2e854a9be394dc6b5ca9655187a0a28fc9d67469
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2022-09-29 15:53:54 +0200

    Added files to git

---
+++ be.MbDevelopment.Greenmaster.Contracts/Dtos/SpecieDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace be.MbDevelopment.Greenmaster.Contracts.Dtos;
+
+public class SpecieDto
+{
+    [DisplayName("Multi-seasonal interest")]
+    public bool IsMultiSeasonInterest { get; set; }
+    [DisplayName("Maintenance-level")]
+    public string MaintenanceLevel { get; set;}
+    [DisplayName("Leaf-color summer")]
+    public string LeafColorSummer { get; set;}
+    [DisplayName("Leaf-color autumn")]
+    public string LeafColorAutumn { get; set;}
+    public string FloweringInfo { get; set;}
+    [DisplayName("Phennology")]
+    public string Cycle { get; set;}
+    [DisplayName("Fragrant")] public bool IsFragrant { get; set; }
+    [DisplayName("Attracts pollinators")] public bool AttractsPollinators { get; set; }
+    [DisplayName("Flower colors")] public int[] FlowerColors { get; set; }
+    [DisplayName("Flowering period")] public int[] FloweringPeriod { get; set; }
+    [DisplayName("Height")]
+    public double MetricHeight { get; set;}
+    [DisplayName("Width")]
+    public double MetricWidth { get; set; }
+    [DisplayName("Length")]
+    public double MetricLength { get; set; }
+}
---
+++ be.MbDevelopment.Greenmaster.Data/GardenContext.cs
@@ -0,0 +1,30 @@
+﻿using be.MbDevelopment.Greenmaster.Data.Extensions;
+using be.MbDevelopment.Greenmaster.Models.Entities.Arboretum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace be.MbDevelopment.Greenmaster.Data;
+
+public class GardenContext : DbContext
+{
+    public GardenContext(DbContextOptions<GardenContext> dbContextOptions) : base(dbContextOptions)
+    {
+        #pragma warning disable EF1001
+        Species = new InternalDbSet<Specie>(this, typeof(Specie).ToString());
+        #pragma warning restore EF1001
+    }
+
+    public override int SaveChanges()
+    {
+        this.AddAuditInfo();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        this.AddAuditInfo();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public DbSet<Specie> Species { get; set; }
+}
